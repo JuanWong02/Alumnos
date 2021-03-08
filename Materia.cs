4fad4b58e52db770da229b7759a0bcea32dbabd9
JuanWong02 <juanwong0211@gmail.com>
2021-03-08 10:36:27 -0700

    Se agrega clase materia

---
+++ Materia.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace alumnos
+{
+    class Materia 
+    {
+        public string nombre;
+        public int semestre;
+
+        public Materia(string nombre, int semestre)
+        {
+            this.nombre = nombre;
+            this.semestre = semestre;
+        }
+      
+    
+    }
+}
